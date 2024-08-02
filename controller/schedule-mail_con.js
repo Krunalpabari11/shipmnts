e3a80d9178b1ee46c3dfb096680225609b0c5482
Krunal Pabari_11 <100471559+Krunalpabari11@users.noreply.github.com>
2024-08-02 11:38:59 +0000

    done

--- controller/schedule-mail_con.js
+++ controller/schedule-mail_con.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import { schedule_mail } from '../modal/schedule_modal.js'
 import { DBConnect } from '../DBconnect.js';
+import { scheduleJob } from 'node-schedule';
+import nodeMailer from 'node-mailer';
 export function schedule(req,res)
 {   DBConnect()
     const {sender,recipient,subject,content,schedule}=req.body
@@ -13,8 +15,56 @@ export function schedule(req,res)
             content:content,
             schedule:schedule
         }).save()
-
+        let transporter = nodemailer.createTransport({
+            service: 'Gmail',
+            auth: {
+                user: 'krunalpabar11@gmail.com',
+                pass: 'Krunal@@@123'
+            }
+        });
         
+        // Email content
+        let mailOptions = {
+            from: 'krunalpabari11@gmail.com',
+            to: recipient,
+            subject:subject,
+            text: content
+        };
+        
+        // Send email
+        transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+        });
+        scheduleJob.scheduleJob('59 23 * * *',function(){
+            let transporter = nodemailer.createTransport({
+                service: 'Gmail',
+                auth: {
+                    user: 'krunalpabar11@gmail.com',
+                    pass: 'Krunal@@@123'
+                }
+            });
+            
+            // Email content
+            let mailOptions = {
+                from: 'krunalpabari11@gmail.com',
+                to: recipient,
+                subject:subject,
+                text: content
+            };
+            
+            // Send email
+            transporter.sendMail(mailOptions, function(error, info){
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log('Email sent: ' + info.response);
+                }
+            });
+        })        
         res.status(200).json({success:"email has been scheduled"})
     }
     catch(e)
